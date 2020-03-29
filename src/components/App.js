0c8fa2df7e086a0c8bd32f277a083fab9a1ab888
john morris <fennymorris@live.co.uk>
2020-03-30 00:04:25 +0100

    asd

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/App.css';
+
+function App() {
+  return (
+    <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+      
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+  );
+}
+
+export default App;
